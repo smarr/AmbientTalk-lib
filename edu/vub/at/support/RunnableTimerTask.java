b0283afd5d82ccd04adc69a44e433327ca638d18
tvcutsem <tvcutsem@c0a7138a-1c4d-0410-81cb-3328cd1d7e1d>
2007-03-27 09:34:50 +0000

    Added a small timer library that symbiotically schedules tasks for execution.
    git-svn-id: https://ambienttalk.googlecode.com/svn/atlib/trunk@419 c0a7138a-1c4d-0410-81cb-3328cd1d7e1d

---
+++ edu/vub/at/support/RunnableTimerTask.java
@@ -0,0 +1,52 @@
+/**
+ * AmbientTalk/2 Project
+ * RunnableTimerTask.java created on 27-mrt-2007 at 10:24:34
+ * (c) Programming Technology Lab, 2006 - 2007
+ * Authors: Tom Van Cutsem & Stijn Mostinckx
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package edu.vub.at.support;
+
+import java.util.TimerTask;
+
+/**
+ * A runnable timer task takes a runnable object as input and uses that
+ * as the body for a scheduled timer task. This enables AmbientTalk/2 code
+ * to symbiotically provide an AmbientTalk object implementing java.lang.Runnable
+ * to serve as a timer task.
+ * 
+ * @author tvcutsem
+ */
+public class RunnableTimerTask extends TimerTask {
+
+	private final Runnable runnable_;
+	
+	public RunnableTimerTask(Runnable r) {
+		runnable_ = r;
+	}
+
+	public void run() {
+		runnable_.run();
+	}
+
+}
