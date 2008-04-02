e0c68903b61d1a751b10fa8996e6cac9ce02144c
tvcutsem <tvcutsem@c0a7138a-1c4d-0410-81cb-3328cd1d7e1d>
2008-04-02 14:53:32 +0000

    Added a unit test to check whether symbiotic calls from Java to AT which return futures automatically suspend the Java thread on the AT future
    git-svn-id: https://ambienttalk.googlecode.com/svn/atlib/trunk@951 c0a7138a-1c4d-0410-81cb-3328cd1d7e1d

---
+++ at/unit/SymbioticFuturesTest.java
@@ -0,0 +1,54 @@
+/**
+ * AmbientTalk/2 Project
+ * SymbioticFuturesTest.java created on 2 apr 2008 at 16:33:54
+ * (c) Programming Technology Lab, 2006 - 2007
+ * Authors: Tom Van Cutsem & Stijn Mostinckx
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package at.unit;
+
+import edu.vub.at.exceptions.InterpreterException;
+
+/**
+ * @author tvcutsem
+ *
+ * TODO document the class SymbioticFuturesTest
+ * 
+ */
+public class SymbioticFuturesTest {
+
+	private interface Cell {
+		public int access();
+	}
+	
+	public static void test(final Cell c, final Runnable cont) throws InterpreterException {
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				int x = c.access(); // access returns an AT future which is resolved later
+				cont.run();
+			}
+		});
+		t.start();
+	}
+	
+}
